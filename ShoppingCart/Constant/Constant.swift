ca0b2a4d486f291d9d088c3597b980ad38b9c570
YeoSungeun <dutjddms@naver.com>
2024-06-13 20:05:06 +0900

    [Chore] UIColor, UIFont Enum 생성,  Asset 추가

--- ShoppingCart/Constant/Constant.swift
+++ ShoppingCart/Constant/Constant.swift
@@ -5,4 +5,24 @@
 //  Created by 여성은 on 6/13/24.
 //
 
-import Foundation
+import UIKit
+
+enum Color {
+    static let mainColor = UIColor(rgb: 0xEF8947)
+    static let black = UIColor(rgb: 0x000000)
+    static let darkgray = UIColor(rgb: 0x4C4C4C)
+    static let gray = UIColor(rgb: 828282)
+    static let lightgray = UIColor(rgb: 0xCDCDCD)
+    static let white = UIColor(rgb: 0xFFFFFF)
+}
+
+enum Font {
+    static let bold16 = UIFont.boldSystemFont(ofSize: 16)
+    static let bold15 = UIFont.boldSystemFont(ofSize: 15)
+    static let bold14 = UIFont.boldSystemFont(ofSize: 14)
+    static let bold13 = UIFont.boldSystemFont(ofSize: 13)
+    
+    static let regular15 = UIFont.systemFont(ofSize: 15)
+    static let regular14 = UIFont.systemFont(ofSize: 14)
+    static let regular13 = UIFont.systemFont(ofSize: 13)
+}
--- ShoppingCart/ReuseIdnetifierProtocol.swift
+++ ShoppingCart/ReuseIdnetifierProtocol.swift
@@ -5,4 +5,27 @@
 //  Created by 여성은 on 6/13/24.
 //
 
-import Foundation
+import UIKit
+
+protocol ReuseIdnetifierProtocol {
+    static var id: String { get }
+}
+
+extension UIViewController: ReuseIdnetifierProtocol {
+    static var id: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReuseIdnetifierProtocol {
+    static var id: String {
+        return String(describing: self)
+    }
+
+}
+
+extension UICollectionViewCell: ReuseIdnetifierProtocol {
+    static var id: String {
+        return String(describing: self)
+    }
+}
