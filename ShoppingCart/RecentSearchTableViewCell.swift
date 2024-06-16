31695da1fd3c6da4fdc0d81217b8f1edb8f7fc6c
YeoSungeun <dutjddms@naver.com>
2024-06-16 19:51:13 +0900

    [Feat] SearchMainView UI구성, 최근검색어 유무에 따른 뷰구성 구현

---
+++ ShoppingCart/RecentSearchTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  RecentSearchTableViewCell.swift
+//  ShoppingCart
+//
+//  Created by 여성은 on 6/16/24.
+//
+
+import UIKit
+import SnapKit
+
+class RecentSearchTableViewCell: UITableViewCell {
+    let clockImgaeView = UIImageView()
+    let searchWordLabel = UILabel()
+    let deleteButton = UIButton()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+        contentView.addSubview(clockImgaeView)
+        contentView.addSubview(searchWordLabel)
+        contentView.addSubview(deleteButton)
+    }
+    func configureLayout() {
+        clockImgaeView.snp.makeConstraints { make in
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).inset(20)
+            make.verticalEdges.equalToSuperview().inset(12)
+            make.width.equalTo(clockImgaeView.snp.height)
+        }
+        searchWordLabel.snp.makeConstraints { make in
+            make.leading.equalTo(clockImgaeView.snp.trailing).offset(16)
+            make.verticalEdges.equalToSuperview()
+            make.trailing.equalTo(deleteButton.snp.leading)
+        }
+        deleteButton.snp.makeConstraints { make in
+            make.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(20)
+            make.verticalEdges.equalToSuperview()
+            
+        }
+    }
+    func configureUI() {
+        
+        clockImgaeView.image = UIImage(systemName: "clock")
+        clockImgaeView.contentMode = .scaleAspectFit
+        clockImgaeView.tintColor = Color.black
+        
+        searchWordLabel.font = Font.regular13
+        
+        deleteButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        deleteButton.tintColor = Color.black
+        deleteButton.addTarget(self, action: #selector(deleteButtonClicked), for: .touchUpInside)
+
+    }
+    func configureCell(data: String) {
+        searchWordLabel.text = data
+    }
+    @objc func deleteButtonClicked() {
+        print(#function)
+    }
+}
--- ShoppingCart/SearchMainViewController.swift
+++ ShoppingCart/SearchMainViewController.swift
@@ -6,24 +6,188 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
+import Kingfisher
 
 class SearchMainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let searchBar = UISearchBar()
+    
+    let noSearchImageView = UIImageView()
+    let noSearchLabel = UILabel()
+    
+    let recentSearchLabel = UILabel()
+    let removeAllSearch = UIButton()
+    let recentSearchTableView = UITableView()
+    
+    var searchList: [String] = []
+    
+    
+    
+    override func loadView() {
+        super.loadView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        searchList.append("dfd")
+//        UserDefaults.standard.set(searchList, forKey: UserDefaultsKey.recentSearch)
+        var recentSearch = UserDefaults.standard.array(forKey: UserDefaultsKey.recentSearch) as? [String] ?? []
+        searchList = recentSearch
+        lazy var count = searchList.count
+        print("viewdidloadcount\(count)")
+ 
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        configureView()
+        
     }
-    */
 
+    
+    func configureHierarchy() {
+        view.addSubview(searchBar)
+        if searchList.count == 0 {
+            view.addSubview(noSearchImageView)
+            view.addSubview(noSearchLabel)
+        } else {
+            view.addSubview(recentSearchLabel)
+            view.addSubview(removeAllSearch)
+            view.addSubview(recentSearchTableView)
+        }
+    }
+    func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        if searchList.count == 0 {
+            noSearchImageView.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.centerY.equalToSuperview().offset(-50)
+                make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+                
+            }
+            noSearchLabel.snp.makeConstraints { make in
+                make.top.equalTo(noSearchImageView.snp.bottom).offset(8)
+                make.centerX.equalToSuperview()
+                make.height.equalTo(20)
+            }
+        } else {
+            recentSearchLabel.snp.makeConstraints { make in
+                make.top.equalTo(searchBar.snp.bottom).offset(8)
+                make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+                make.height.equalTo(20)
+            }
+            removeAllSearch.snp.makeConstraints { make in
+                make.bottom.equalTo(recentSearchLabel.snp.bottom)
+                make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
+                make.height.equalTo(20)
+            }
+            recentSearchTableView.snp.makeConstraints { make in
+                make.top.equalTo(recentSearchLabel.snp.bottom).offset(8)
+                make.horizontalEdges.bottom.equalToSuperview()
+            }
+            
+        }
+    }
+    func configureUI() {
+        view.backgroundColor = Color.white
+        guard let nickname = UserDefaults.standard.string(forKey: UserDefaultsKey.UserNickname) else { return }
+        navigationItem.title = "\(nickname)'s MEANING OUT"
+        
+        searchBar.placeholder = "브랜드, 상품 등을 입력하세요"
+        if searchList.count == 0 {
+            noSearchImageView.image = UIImage.empty
+            noSearchLabel.contentMode = .scaleAspectFill
+            noSearchLabel.text = "최근 검색어가 없어요"
+            noSearchLabel.font = Font.bold14
+            noSearchLabel.textAlignment = .center
+        } else {
+            recentSearchLabel.text = "최근 검색"
+            recentSearchLabel.textColor = Color.black
+            recentSearchLabel.font = Font.bold14
+            
+            removeAllSearch.setTitle("전체 삭제", for: .normal)
+            removeAllSearch.setTitleColor(Color.mainColor, for: .normal)
+            removeAllSearch.titleLabel?.font = Font.regular13
+            removeAllSearch.addTarget(self, action: #selector(removeAllButtonClicked), for: .touchUpInside)
+            
+        }
+
+    }
+    func configureView() {
+        searchBar.delegate = self
+        
+        if searchList.count != 0 {
+            recentSearchTableView.delegate = self
+            recentSearchTableView.dataSource = self
+            recentSearchTableView.register(RecentSearchTableViewCell.self, forCellReuseIdentifier: RecentSearchTableViewCell.id)
+            
+            recentSearchTableView.rowHeight = 40
+        }
+
+    }
+    
+    @objc func removeAllButtonClicked() {
+        UserDefaults.standard.removeObject(forKey: UserDefaultsKey.recentSearch)
+        searchList = []
+        self.loadView()
+        self.viewDidLoad()
+    }
 }
+
+extension SearchMainViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(#function)
+        // 최근검색어 저장
+        var udSearchList = UserDefaults.standard.array(forKey: UserDefaultsKey.recentSearch) as? [String] ?? []
+        print(udSearchList)
+        guard let text = searchBar.text else { return }
+        print(text)
+        udSearchList.insert(text, at: 0)
+        searchList = udSearchList
+        print(udSearchList, searchList)
+        UserDefaults.standard.set(searchList, forKey: UserDefaultsKey.recentSearch)
+        
+        print(searchList.count)
+        if searchList.count == 1 {
+            self.loadView()
+            self.viewDidLoad()
+        }
+        recentSearchTableView.reloadData()
+        
+        navigationController?.pushViewController(SearchResultViewController(), animated: true)
+        
+        view.endEditing(true)
+    }
+}
+
+extension SearchMainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RecentSearchTableViewCell.id, for: indexPath) as! RecentSearchTableViewCell
+        let data = searchList[indexPath.row]
+        cell.configureCell(data: data)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(#function, indexPath)
+    }
+    
+    
+}
+
+
+#if DEBUG
+@available (iOS 17, *)
+#Preview {
+    SearchMainViewController()
+}
+#endif
