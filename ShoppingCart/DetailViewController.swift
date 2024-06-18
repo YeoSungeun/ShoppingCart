cb98514fd71c392c103e2db4ef0b7f809e513263
YeoSungeun <dutjddms@naver.com>
2024-06-18 15:42:42 +0900

    [Feat] DetailView 구성

--- ShoppingCart/DetailViewController.swift
+++ ShoppingCart/DetailViewController.swift
@@ -6,23 +6,66 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
 
+    let webView = WKWebView()
+    
+    var itemName = ""
+    var itemId = ""
+    var itemURLString = "https://www.naver.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        configureNavigationButton()
+    
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureHierarchy() {
+        view.addSubview(webView)
+    }
+    func configureLayout() {
+        webView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    func configureUI() {
+        view.backgroundColor = .white
+        let itemNameLabel = UILabel()
+        itemNameLabel.attributedText = itemName.htmlToAttributedString
+        navigationItem.title = itemNameLabel.text
+        
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonClicked))
+        backButton.tintColor = Color.darkgray
+        navigationItem.leftBarButtonItem = backButton
+        
+        let url = URL(string: "\(itemURLString)") ?? URL(string: "https://www.naver.com")!
+        // url에 연산프로퍼티~
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+    }
+    func configureNavigationButton() {
+        let likeList = UserDefaults.standard.array(forKey: UserDefaultsKey.likeList) as? [String] ?? []
+        
+        if likeList.contains(itemId) {
+            let likeButton = UIBarButtonItem(image: UIImage.likeSelected, style: .plain, target: self, action: #selector(likeButtontoggled))
+            
+            navigationItem.rightBarButtonItem = likeButton
+        } else {
+            let likeButton = UIBarButtonItem(image: UIImage.likeUnselected, style: .plain, target: self, action: #selector(likeButtontoggled))
+            likeButton.tintColor = .clear
+            navigationItem.rightBarButtonItem = likeButton
+        }
+    }
+    @objc func backButtonClicked() {
+        navigationController?.popViewController(animated: true)
+    }
     }
     */
 
